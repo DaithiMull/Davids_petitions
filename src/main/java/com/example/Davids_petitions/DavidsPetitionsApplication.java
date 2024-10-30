a12c31c0b4b1b002f302005d9661bad9b0a79c91
DaithiMull <workdmullane@gmail.com>
2024-10-30 09:29:39 +0000

    1st commit

---
+++ src/main/java/com/example/Davids_petitions/DavidsPetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Davids_petitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidsPetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidsPetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Davids_petitions/DavidsPetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Davids_petitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavidsPetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
